e15380cf85b7465146534782013e33c147e405d3
varshneypiyush19 <varshneypiyush2004@gmail.com>
2024-11-29 20:53:06 +0530

    list of task fixed

--- controllers/task.js
+++ controllers/task.js
@@ -18,7 +18,7 @@ export const newTask = async (req, res, next) => {
 export const getMyTask = async (req, res, next) => {
   try {
     const userId = req.user._id;
-    const tasks = await Task.findOne({ user: userId });
+    const tasks = await Task.find({ user: userId });
 
     res.status(200).json({
       success: true,
